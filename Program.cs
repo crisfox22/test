7b230e0764187b43fc0674e77414b970fc20fb21
Cristiano Santos <cristianopsantos2020@gmail.com>
2022-10-22 21:00:04 +0100

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestandoGithub
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // testando o Github com o Visual Studio 
+        }
+    }
+}
